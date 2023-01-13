e90ce73255541d3b5ee9f8fe4914829618e205d3
Mae Schreck <maedevlin47@gmail.comm>
2023-01-13 18:10:59 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,90 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
 puts "Clearing DB"
+
 User.destroy_all
 Favorite.destroy_all
 Service.destroy_all
+
 puts "DB cleared!"
 
+
 puts "Creating Users"
+
 10.times {User.create(
-    name: Faker::Celebrity::Name.full_name, 
-    username:Faker::Name.first_name, 
-    birthdate: Faker::Date.between(from: '1930-01-01', to: '2023-01-01'), 
-    city: Faker::City.city_name,
-    state: Faker::State.state_abbr,
+    name: Faker::Name.unique.name, 
+    username: Faker::Internet.username(specifier:7),
+    birthdate: Faker::Date.birthday(min_age: 18, max_age: 100), 
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
     password: "1")}
-
-
-
-
+    # user_id: Faker::Number.between(from: 0, to: 10)
 
 puts "Creating Services"
+
 service1 = Service.create(name: "Uber", travel_type: "Car Service")
+service2 = Service.create(name: "Lyft", travel_type: "Car Service")
+service3 = Service.create(name: "Via", travel_type: "Car Service")
+service4 = Service.create(name: "Yellow Cab", travel_type: "Car Service")
+service5 = Service.create(name: "Gojek", travel_type: "Car Service")
+service6 = Service.create(name: "Revel", travel_type: "Car Service")
+service7 = Service.create(name: "Hitch", travel_type: "Car Service")
+service8 = Service.create(name: "Transit", travel_type: "Train & Bus")
+service9 = Service.create(name: "Amtrak", travel_type: "Train")
+service10 = Service.create(name: "MTA", travel_type: "")
+service11 = Service.create(name: "Metro Transit", travel_type: "Train & Bus")
+service12 = Service.create(name: "Hopper", travel_type: "Flight")
+service13 = Service.create(name: "Kiwi.com", travel_type: "Flight")
+service14 = Service.create(name: "Kayak", travel_type: "Flight")
+service15 = Service.create(name: "SkyScanner", travel_type: "Flight")
 
-
-
+puts "Created Serice"
 
 
 puts "Creating Favorites"
+
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service1.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service2.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service3.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service4.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service5.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service6.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service7.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service8.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service9.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service10.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service11.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service12.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service13.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service14.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service15.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service11.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service8.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service6.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service3.id)
+Favorite.create(user_id: Faker::Number.between(from: 0, to: 10), service_id: service5.id)
+
+puts "Created Favorite"
+
+
+
+puts "Done Seeding"
+
+# Favorite.create(user_id: user2.id, service_id: service2.id)
+# Favorite.create(user_id: user3.id, service_id: service3.id)
+# Favorite.create(user_id: user4.id, service_id: service4.id)
+# Favorite.create(user_id: user5.id, service_id: service5.id)
+# Favorite.create(user_id: user6.id, service_id: service6.id)
+# Favorite.create(user_id: user7.id, service_id: service7.id)
+# Favorite.create(user_id: user8.id, service_id: service8.id)
+# Favorite.create(user_id: user9.id, service_id: service9.id)
+# Favorite.create(user_id: user10.id, service_id: service10.id)
+# Favorite.create(user_id: user3.id, service_id: service11.id)
+# Favorite.create(user_id: user2.id, service_id: service12.id)
+# Favorite.create(user_id: user1.id, service_id: service13.id)
+# Favorite.create(user_id: user4.id, service_id: service14.id)
+# Favorite.create(user_id: user5.id, service_id: service15.id)
+# Favorite.create(user_id: user6.id, service_id: service16.id)
+# Favorite.create(user_id: user7.id, service_id: service17.id)
+# Favorite.create(user_id: user8.id, service_id: service18.id)
+# Favorite.create(user_id: user9.id, service_id: service3.id)
+# Favorite.create(user_id: user10.id, service_id: service5.id)
