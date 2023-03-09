9215406e35bcb6eaee561d60c708b49c75955972
Mae Schreck <maedevlin47@gmail.comm>
2023-03-09 17:40:16 -0500

    reseeding

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,5 @@
 class Service < ApplicationRecord
     has_many :favorites, dependent: :destroy
     has_many :users, through: :favorites
-    attr_accessor :description
-    attr_accessor :signup
-    attr_accessor :features
+    attr_accessor :description, :signup, :features
 end
